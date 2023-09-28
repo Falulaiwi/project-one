a19d0be3821e66ab26ff19668dcc2c409c906921
Fawaz Alulaiwi <146366794+Falulaiwi@users.noreply.github.com>
2023-09-28 20:27:23 +0300

    Add files via upload

---
+++ win/parts/footer.php
@@ -0,0 +1,8 @@
+</div>
+<script src="./js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
+<script src="./JS/loading.js"></script>
+<script src="./JS/script.js"></script>
+</body>
+
+</html>
