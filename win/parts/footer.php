e6eb776ab40fe8834ddeb1f252239f9641995e87
Fawaz Alulaiwi <146366794+Falulaiwi@users.noreply.github.com>
2023-09-28 20:26:08 +0300

    Delete win directory

--- win/parts/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-<script src="./js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
-<script src="./JS/loading.js"></script>
-<script src="./JS/script.js"></script>
-</body>
-
-</html>
